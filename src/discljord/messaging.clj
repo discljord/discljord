e27692f60a7e0ebf8d1b02a28c6fc366d57eca93
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-03-12 16:30:42 -0400

    Change kind of zero-width space to prevent phantom newlines.

--- src/discljord/messaging.clj
+++ src/discljord/messaging.clj
@@ -44,7 +44,7 @@
   (http/post (conn/api-url (str "/channels/" channel-id "/messages"))
              {:headers {"Authorization" (:token bot)
                         "Content-Type" "application/json"}
-              :body (json/write-str {:content (str \u200B content)})}))
+              :body (json/write-str {:content (str \u180E content)})}))
 
 (defn get-channel
   [bot channel-id]

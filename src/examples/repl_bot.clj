a4a9c9d48e9c26e76d3a2e9ad24a86d281066dae
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-03-13 15:55:03 -0400

    Add support for a real repl.

--- src/examples/repl_bot.clj
+++ src/examples/repl_bot.clj
@@ -6,7 +6,8 @@
             [clojure.core.async :as a]
             [clojure.java.io :as io]
             [clojure.edn :as edn]
-            [clojure.pprint :as pp])
+            [clojure.pprint :as pp]
+            [clojure.main :refer [repl]])
   (:import [org.eclipse.jetty websocket.client.WebSocketClient util.ssl.SslContextFactory])
   (:use com.rpl.specter))
 
@@ -21,6 +22,46 @@
   (a/>!! (:event-channel bot) {:event-type :disconnect :event-data nil})
   (transform [:shards ALL :socket-state] #(when % (conn/disconnect-websocket %)) bot))
 
+(defn start-repl
+  "Starts a repl that will send output to the given discord channel which gives
+  no prompts, and takes no input directly from said channel. Instead it will
+  take all input as strings from the core.async channel passed as read-chan.
+  When the keyword :stop is put on read-chan, the repl will exit."
+  [bot channel-id read-chan]
+  (let [messages (atom [])]
+    (a/go
+      (repl :need-prompt (constantly true)
+            :prompt #()
+            :flush #(do
+                      (doseq [message @messages]
+                        (m/send-message bot channel-id message))
+                      (reset! messages []))
+            :read (fn [prompt exit]
+                    (let [msg (a/<!! read-chan)]
+                      (condp = msg
+                        :stop exit
+                        (read-string msg))))
+            :eval #(let [return-result (atom nil)
+                         print-result (with-out-str (reset! return-result (eval %)))
+                         print-result (when-not (= print-result "")
+                                        (str "```\r\n" print-result "\r\n```"))]
+                     (when print-result
+                       (swap! messages conj print-result))
+                     @return-result)
+            :print (fn [& more]
+                     (let [more (map #(str "```\r\n" (if % % "nil") "\r\n```") more)]
+                       (apply swap! messages conj more)))
+            :caught (fn [throwable]
+                      (let [exception-text (with-out-str
+                                             (binding [*err* *out*]
+                                               (.printStackTrace throwable)))
+                            exception-text (if (> (count exception-text) 1500)
+                                             (subs exception-text 0 1500)
+                                             exception-text)]
+                        (swap! messages
+                               conj (str "Exception thrown:\r\n```\r\n"
+                                         exception-text "\r\n```"))))))))
+
 (defn repl-command
   [s]
   (let [return-result (atom nil)
@@ -34,6 +75,9 @@
                       (subs print-result 0 1500)
                       print-result)]))
 
+(def real-repl (atom false))
+(def channel-readers (atom {}))
+
 (defn proc-command
   [bot {event-type :event-type
         {:keys [channel-id content type mentions]
@@ -42,14 +86,21 @@
                                        end
                                        (count content)))]
     (condp = command-type
-      "repl>" (let [[return-result print-result]
-                    (repl-command (subs content (inc (str/index-of content " "))))]
-                (m/send-message bot channel-id (str "```\r\n"
-                                                    print-result
-                                                    "\r\n```"))
-                (m/send-message bot channel-id (str "```\r\n; => "
-                                                    (with-out-str (prn return-result))
-                                                    "\r\n```")))
+      "repl>" (if @real-repl
+                (do
+                  (when-not (get @channel-readers channel-id)
+                    (swap! channel-readers assoc channel-id (a/chan 100)))
+                  (a/>!! (get @channel-readers channel-id) (subs content (inc (str/index-of content " ")))))
+                (let [[return-result print-result]
+                      (repl-command (subs content (inc (str/index-of content " "))))]
+                  (when-not (= print-result "")
+                    (m/send-message bot channel-id (str "```\r\n"
+                                                        print-result
+                                                        "\r\n```")))
+                  (Thread/sleep 50)
+                  (m/send-message bot channel-id (str "```\r\n; => "
+                                                      (with-out-str (prn return-result))
+                                                      "\r\n```"))))
       "!disconnect" (disconnect bot)
       nil)))
 

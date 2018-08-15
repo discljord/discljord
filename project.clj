e0e3b19e5fdeae0986eaf8ab380d5c5fdf8f8a22
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-08-15 19:54:07 -0400

    Move dependencies to appropriate areas

--- project.clj
+++ project.clj
@@ -6,12 +6,11 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [http-kit "2.2.0"]
-                 [http-kit.fake "0.2.2"]
                  [stylefruits/gniazdo "1.0.1"]
-                 [org.eclipse.jetty.websocket/websocket-client "9.4.6.v20170531"]
                  [org.clojure/core.async "0.3.442"]
                  [com.rpl/specter "1.0.5"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[http-kit.fake "0.2.2"]]}}
   :jvm-opts ["-Dorg.eclipse.jetty.websocket.client.LEVEL=WARN"
              "--add-modules" "java.xml.bind"])

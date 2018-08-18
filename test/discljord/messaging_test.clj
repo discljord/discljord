ffde45172b2c15dc0e34dd462f23d565fa5f76fe
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-08-18 17:33:28 -0400

    Remove files in preparation for redesign

--- test/discljord/messaging_test.clj
+++
@@ -1,8 +0,0 @@
-(ns discljord.messaging-test
-  (:require [discljord.messaging :refer :all]
-            [clojure.test :as t]))
-
-(t/deftest mentions
-  (t/testing "Are user mentions properly created?"
-    (t/is (= "<@1701>"
-             (mention 1701)))))

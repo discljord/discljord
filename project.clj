d207cd8226371e36b643951201ee6988ac694f7a
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-09-23 16:40:08 -0400

    Update dependendies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [http-kit "2.2.0"]
                  [stylefruits/gniazdo "1.0.1"]
-                 [org.clojure/core.async "0.3.442"]
-                 [com.rpl/specter "1.0.5"]
-                 [org.clojure/tools.logging "0.4.1"]]
+                 [com.rpl/specter "1.1.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[http-kit.fake "0.2.2"]

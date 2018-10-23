f3b262d894b9fb4641b7af1e03407277b4d0d556
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-10-23 02:39:17 -0400

    Revert premature change of specs

--- src/discljord/connections/specs.clj
+++ src/discljord/connections/specs.clj
@@ -4,39 +4,21 @@
             [discljord.specs :as ds]))
 
 ;; ---------------------------------------------------
-;; Domain Model
-
-(s/def ::heartbeat-interval (s/nilable number?))
-(s/def ::last-heartbeat (s/nilable number?))
-(s/def ::connection any?)
-(s/def ::connection-state (ds/atom-of? #{::connected ::disconnected}))
-(s/def ::messages ::ds/channel)
-(s/def ::shard (s/keys :req-un [::heartbeat-interval ::last-heartbeat
-                                ::connection ::connection-state
-                                ::messages]))
-
-(s/def ::shards (s/coll-of ::shard))
+(s/def ::shard-id int?)
 (s/def ::shard-count pos-int?)
 (s/def ::gateway (s/keys :req [::ds/url ::shard-count]))
-(s/def ::bot-connection (s/keys :req-un [::ds/token ::shards]
-                                :opt-un [::gateway]))
 
-(s/def ::op-code int?)
-(s/def ::message-data any?)
-(s/def ::session string?)
-(s/def ::seq int?)
-(s/def ::message (s/keys :req [::op-code ::message-data]
-                         :opt [::seq ::session]))
+(s/def ::session-id (s/nilable string?))
+(s/def ::seq (s/nilable int?))
+(s/def ::buffer-size number?)
+(s/def ::disconnect boolean?)
+(s/def ::max-connection-retries number?)
+(s/def ::shard-state (s/keys :req-un [::session-id ::seq
+                                      ::buffer-size ::disconnect
+                                      ::max-connection-retries]))
+(s/def ::init-shard-state ::shard-state)
 
-(s/def ::event-type keyword?)
-(s/def ::event-data any?)
-(s/def ::event (s/keys :req [::event-type ::event-data]))
-
-(s/def ::command-type #{::disconnect ::update-status
-                        ::update-voice-state
-                        ::request-guild-members})
-(s/def ::command-data any?)
-(s/def ::command (s/keys :req [::command-type ::command-data]))
+(s/def ::connection any?)
 
 ;; ---------------------------------------------------
 ;; Specific command specs

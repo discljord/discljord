239a51ddedbc44cffc4a4e61ae38cc6f23a71605
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2019-03-01 21:11:14 -0600

    Fix bug with dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.suskalo/discljord "0.2.2"
+(defproject org.suskalo/discljord "0.2.3"
   :description " A Clojure wrapper library for the Discord API, with full API coverage (except voice, for now), and high scalability."
   :url "https://github.com/IGJoshua/discljord"
   :license {:name "Eclipse Public License"
@@ -8,8 +8,8 @@
                  [org.clojure/data.json "0.2.6"]
                  [com.taoensso/timbre "4.10.0"]
                  [http-kit "2.4.0-alpha3"]
-                 [stylefruits/gniazdo "1.0.1"]
-                 [com.rpl/specter "1.1.1"]]
+                 [stylefruits/gniazdo "1.1.1"]
+                 [com.rpl/specter "1.1.2"]]
   :target-path "target/%s"
   :jar-name "discljord-%s.jar"
   :deploy-branches ["master" "release"]

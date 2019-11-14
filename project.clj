48ac67161bee6b9a0afa1b81dc7f10c3caab0fa7
Joshua Suskalo <joshua@suskalo.org>
2019-11-13 23:18:42 -0600

    Upgrade clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/IGJoshua/discljord"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.490"]
                  [org.clojure/data.json "0.2.6"]
                  [com.taoensso/timbre "4.10.0"]

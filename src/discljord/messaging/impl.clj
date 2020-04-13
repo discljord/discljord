63653d79602db76bd16eb64af34645f44ce25cd3
Joshua Suskalo <joshua@suskalo.org>
2020-04-13 09:57:07 -0500

    Version bump to snapshot

--- src/discljord/messaging/impl.clj
+++ src/discljord/messaging/impl.clj
@@ -36,7 +36,7 @@
    (str "DiscordBot ("
         "https://github.com/IGJoshua/discljord"
         ", "
-        "0.2.8"
+        "0.2.9-SNAPSHOT"
         ") "
         user-agent)
    "Content-Type" "application/json"})

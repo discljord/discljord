32907b62a313d69f3be65a2a466a735edc250bcd
Joshua Suskalo <joshua@suskalo.org>
2020-06-29 13:25:21 -0500

    Add skeleton for caching middleware

--- src/discljord/events/state.clj
+++ src/discljord/events/state.clj
@@ -1,2 +1,18 @@
 (ns discljord.events.state
-  "Middleware to cache all the state that Discord sends.")
+  "Middleware to cache all the state that Discord sends."
+  (:require
+   [clojure.tools.logging :as log]
+   [discljord.events :as e]
+   [discljord.events.middleware :as mdw]))
+
+(def ^:private caching-handlers
+  "Handler map for all state-caching events."
+  {})
+
+(defn caching-middleware
+  "Creates a middleware that caches all Discord event data in `state`.
+
+  `state` must be an [[clojure.core/atom]]."
+  [state]
+  (mdw/concat
+   #(e/dispatch-handlers #'caching-handlers %1 %2 state)))

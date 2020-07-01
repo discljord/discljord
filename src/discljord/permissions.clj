be8dfd29ce7a5e8c4ace05b205b81e0a19792ea2
Joshua Suskalo <joshua@suskalo.org>
2020-07-01 14:49:16 -0500

    Add permissions bit map

--- src/discljord/permissions.clj
+++ src/discljord/permissions.clj
@@ -1,2 +1,36 @@
 (ns discljord.permissions
   "Functions for determining users' permissions.")
+
+(def permissions-bit
+  "Map from permission names to the binary flag representation of it."
+  {:create-instant-invite 0x1
+   :kick-members 0x2
+   :ban-members 0x4
+   :administrator 0x8
+   :manage-channels 0x10
+   :manage-guild 0x20
+   :add-reactions 0x40
+   :view-audit-log 0x80
+   :priority-speaker 0x100
+   :stream 0x200
+   :view-channel 0x400
+   :send-messages 0x800
+   :send-tts-messages 0x1000
+   :manage-messages 0x2000
+   :embed-links 0x4000
+   :attach-files 0x8000
+   :read-message-history 0x10000
+   :mention-everyone 0x20000
+   :use-external-emojis 0x40000
+   :view-guild-insights 0x80000
+   :connect 0x100000
+   :speak 0x200000
+   :mute-members 0x400000
+   :deafen-members 0x800000
+   :move-members 0x1000000
+   :use-vad 0x2000000
+   :change-nickname 0x4000000
+   :manage-nicknames 0x8000000
+   :manage-roles 0x10000000
+   :manage-webooks 0x20000000
+   :manage-emojis 0x40000000})

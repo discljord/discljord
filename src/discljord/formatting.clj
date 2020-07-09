ee80f337e6d0ca356a246d5392b6b4ddadde2f82
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-07-09 17:50:52 +0200

    Add docstrings

--- src/discljord/formatting.clj
+++ src/discljord/formatting.clj
@@ -1,18 +1,29 @@
-(ns discljord.formatting)
+(ns discljord.formatting
+  "Contains utility functions to help with Discord message formatting.")
 
 (defn- extract-id [entity]
   (cond-> entity (map? entity) :id))
 
-(defn mention-user [user]
+(defn mention-user
+  "Takes a user object or id and returns a mention of that user for use in messages."
+  [user]
   (str "<@" (extract-id user) \>))
 
-(defn mention-role [role]
+(defn mention-role
+  "Takes a role object or id and returns a mention of that role for use in messages."
+  [role]
   (str "<@&" (extract-id role) \>))
 
-(defn mention-channel [channel]
+(defn mention-channel
+  "Takes a text channel object or id and returns a mention of that channel for use in messages."
+  [channel]
   (str "<#" (extract-id channel) \>))
 
-(defn mention-emoji [emoji]
+(defn mention-emoji
+  "Takes an emoji object or a custom emoji id and returns a mention of that emoji for use in messages.
+  A provided emoji object may also represent a regular unicode emoji with just a name,
+  in which case that name will be returned."
+  [emoji]
   (if (map? emoji)
     (let [{:keys [animated name id]} emoji]
       (if id
@@ -20,5 +31,7 @@
         name))
     (str "<:_:" emoji \>)))
 
-(defn user-tag [{:keys [username discriminator] :as user}]
+(defn user-tag
+  "Takes a user object and returns a string representing it as a tag, i.e. \"username#discriminator\"."
+  [{:keys [username discriminator] :as user}]
   (str username \# discriminator))

115c31c567b2e80f2277641ed611ebae59ca77dc
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-07-09 17:23:50 +0200

    Add formatting namespace with mention functions

---
+++ src/discljord/formatting.clj
@@ -0,0 +1,21 @@
+(ns discljord.formatting)
+
+(defn- extract-id [entity]
+  (cond-> entity (map? entity) :id))
+
+(defn mention-user [user]
+  (str "<@" (extract-id user) \>))
+
+(defn mention-role [role]
+  (str "<@&" (extract-id role) \>))
+
+(defn mention-channel [channel]
+  (str "<#" (extract-id channel) \>))
+
+(defn mention-emoji [emoji]
+  (if (map? emoji)
+    (let [{:keys [animated name id]} emoji]
+      (if id
+        (str \< (if animated \a "") \: name \: id \>)
+        name))
+    (str "<:_:" emoji \>)))

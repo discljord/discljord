1a8afc139daf5314f5f8b4c1bcdad8d00273b5a0
Joshua Suskalo <joshua@suskalo.org>
2020-07-09 22:24:14 -0500

    Add docstrings to permissions functions

--- src/discljord/permissions.clj
+++ src/discljord/permissions.clj
@@ -36,16 +36,19 @@
    :manage-emojis 0x40000000})
 
 (defn has-permission-flag?
+  "Returns if the given permission integer includes a permission flag."
   [perm perms-int]
   (when perms-int
     (when-let [bit (permissions-bit perm)]
       (not (zero? (bit-and bit perms-int))))))
 
 (defn has-permission-flags?
+  "Returns if the given permission integer includes all the given permission flags."
   [perms perms-int]
   (every? #(has-permission-flag? % perms-int) perms))
 
 (defn permission-int
+  "Constructs a permissions integer given a set of roles and overrides."
   ([everyone roles]
    (let [perms-int (reduce bit-or 0 (conj roles everyone))]
      (if (has-permission-flag? :administrator perms-int)
@@ -67,10 +70,17 @@
      (override perms-int user-overrides))))
 
 (defn- user-roles
+  "Returns a sequence of permissions integers for a user's roles."
   [guild user-id]
   (map :permissions (vals (select-keys (:roles guild) (:roles ((:members guild) user-id))))))
 
 (defn has-permission?
+  "Returns if the given user has a permission.
+
+  `everyone` and `everyone-overrides` are permissions integers; `roles`,
+  `role-overrides`, and `user-overrides` are sequences of permissions integers.
+
+  `guild` is a guild object like those from [[discljord.events.state]]."
   {:arglists '([perm everyone roles] [perm guild user-id] [perm guild user-id channel-id]
                [perm everyone roles everyone-overrides roles-overrides user-overrides])}
   ([perm everyone-or-guild roles-or-user-id]
@@ -90,6 +100,12 @@
     (permission-int everyone roles everyone-overrides roles-overrides user-overrides))))
 
 (defn has-permissions?
+  "Returns if the given user has each of a sequence of permissions.
+
+  `everyone` and `everyone-overrides` are permissions integers; `roles`,
+  `role-overrides`, and `user-overrides` are sequences of permissions integers.
+
+  `guild` is a guild object like those from [[discljord.events.state]]."
   {:arglists '([perms everyone roles] [perms guild user-id] [perms guild user-id channel-id]
                [perms everyone roles everyone-overrides roles-overrides user-overrides])}
   ([perms everyone-or-guild roles-or-user-id]

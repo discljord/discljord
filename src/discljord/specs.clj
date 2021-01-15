d2ebd2c085546d6c15e9225b05fbef81f74eee66
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-01-15 15:44:31 +0100

    Fix snowflake spec

--- src/discljord/specs.clj
+++ src/discljord/specs.clj
@@ -28,7 +28,7 @@
     (and (instance? clojure.lang.Agent x)
          (s/valid? s @x))))
 
-(s/def ::snowflake (partial re-matches #"\d+"))
+(s/def ::snowflake (s/or :number int? :sf-string (s/and string? (partial re-matches #"\d+"))))
 
 (s/def ::id ::snowflake)
 (s/def ::channel-id ::snowflake)

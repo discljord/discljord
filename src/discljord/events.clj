819daef5a9befcf1e20c17e9e55d35023bba416e
Joshua Suskalo <joshua@suskalo.org>
2021-06-19 21:35:47 -0500

    Update the grinder to add the event data into the context

--- src/discljord/events.clj
+++ src/discljord/events.clj
@@ -71,22 +71,24 @@
   which must happen next, and an `:fx` key to represent actions which must be
   taken.
 
-  When each event is received from Discord, it is added to the end of the
-  `:queue` in the context in the form [event-type event-data], and then calls
-  `stepper` with the context and each element of the `:queue` in order. The
-  return value from the stepper is the next context, and any alterations to the
-  queue will affect what the stepper will be called next with. After each step,
-  the `:fx` key will be taken and the `fx` function will be called with the
-  context and each element of the collection in `:fx`. When the `:queue` is
-  empty, the event has been fully processed and the modified context will be
-  discarded. The stepper may also return a channel or implementation
-  of [[IDeref]] (although a channel requires fewer threads) that resolves to the
-  next context."
+  When each event is received from Discord, it is added to the context under the
+  keys `:event-type` and `:event-data`, and then `stepper` is called with the
+  context and each element of the `:queue` in order. The return value from the
+  stepper is the next context, and any alterations to the queue will affect what
+  the stepper will be called next with. After each step, the `:fx` key will be
+  taken and the `fx` function will be called with the context and each element
+  of the collection in `:fx`, in parallel. `fx` should not block, and the return
+  value is ignored. When the `:queue` is empty, the event has been fully
+  processed and the modified context will be discarded. The stepper may also
+  return a channel or implementation of [[IDeref]] (although a channel requires
+  fewer threads) that resolves to the next context."
   [event-ch context stepper fx]
   (loop []
-    (let [event (a/<!! event-ch)]
+    (let [[event-type event-data] (a/<!! event-ch)]
       (a/go
-        (loop [ctx (update context :queue concat event)]
+        (loop [ctx (assoc context
+                          :event-type event-type
+                          :event-data event-data)]
           (when-some [[step & next-queue] (seq (:queue ctx))]
             (let [ctx (try
                         (stepper (assoc ctx :queue next-queue) step)
@@ -103,13 +105,15 @@
                         (catch Exception e
                           (log/error e "Exception while dereferencing an async context.")))]
               (when ctx
-                (run! (fn [v]
-                        (try (fx (dissoc ctx :fx) v)
-                             (catch Exception e
-                               (log/error e "Exception while realizing effect."))))
+                (run! (let [ctx (dissoc ctx :fx)]
+                        (fn [v]
+                          (a/go
+                            (try (fx ctx v)
+                                 (catch Exception e
+                                   (log/error e "Exception while realizing effect."))))))
                       (:fx ctx))
                 (recur (dissoc ctx :fx)))))))
-      (when-not (= (first event) :disconnect)
+      (when-not (= event-type :disconnect)
         (recur))))
   nil)
 (s/fdef event-grinder!
